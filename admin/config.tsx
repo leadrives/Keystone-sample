f5399b23ededd53014692f2785504a0f0278284e
leadrives <leadrives@gmail.com>
2025-04-09 19:43:47 +0530

    admin ui logo image dunis

--- admin/config.tsx
+++ admin/config.tsx
@@ -5,10 +5,8 @@ import React from 'react';
 function CustomLogo() {
   return (
     <img
-      src="/images/dunes_logo.a0f45c42.svg" // Path to your custom logo
+      src="/images/logo33.svg" // Path to your custom logo
       alt="My Custom Logo"
-      
-      
     />
     
   );
